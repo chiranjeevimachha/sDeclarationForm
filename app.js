395d6d0326d4c9839e18d9157a0a5dd7785b4636
chiranjeevimachha <chirumj@gmail.com>
2020-09-11 10:22:06 +0530

    third commit

--- app.js
+++ app.js
@@ -9,26 +9,26 @@ var fonts = {
   Roboto: {
     normal: path.join(
       __dirname,
-      "..",
-      "sDeclare/example",
+
+      "/example",
       "/Roboto/Roboto-Regular.ttf"
     ),
     bold: path.join(
       __dirname,
-      "..",
-      "sDeclare/example",
+
+      "/example",
       "/Roboto/Roboto-Medium.ttf"
     ),
     italics: path.join(
       __dirname,
-      "..",
-      "sDeclare/example",
+
+      "/example",
       "/Roboto/Roboto-Italic.ttf"
     ),
     bolditalics: path.join(
       __dirname,
-      "..",
-      "sDeclare/example",
+
+      "/example",
       "/Roboto/Roboto-MediumItalic.ttf"
     ),
   },
